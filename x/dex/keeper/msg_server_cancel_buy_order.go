e6b557bc09f91c378fea9127652582d68cea91d9
mitchellnel <mitchellmclin@gmail.com>
2022-07-28 12:40:48 +0800

    Implement cancellation for buy and sell orders

--- x/dex/keeper/msg_server_cancel_buy_order.go
+++ x/dex/keeper/msg_server_cancel_buy_order.go
@@ -2,16 +2,59 @@ package keeper
 
 import (
 	"context"
+	"errors"
+
+	"interchange-nel/x/dex/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"interchange-nel/x/dex/types"
 )
 
-func (k msgServer) CancelBuyOrder(goCtx context.Context, msg *types.MsgCancelBuyOrder) (*types.MsgCancelBuyOrderResponse, error) {
+func (k msgServer) CancelBuyOrder(
+	goCtx context.Context,
+	msg *types.MsgCancelBuyOrder,
+) (*types.MsgCancelBuyOrderResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	// retrieve the book
+	pairIndex := types.OrderBookIndex(msg.Port, msg.Channel, msg.AmountDenom, msg.PriceDenom)
+	b, found := k.GetBuyOrderBook(ctx, pairIndex)
+	if !found {
+		return &types.MsgCancelBuyOrderResponse{}, errors.New("The pair doesn't exist")
+	}
+
+	// check order creator
+	order, err := b.Book.GetOrderFromID(msg.OrderID)
+	if err != nil {
+		return &types.MsgCancelBuyOrderResponse{}, err
+	}
+
+	if order.Creator != msg.Creator {
+		return &types.MsgCancelBuyOrderResponse{}, errors.New("Canceller must be creator")
+	}
+
+	// remove order
+	if err := b.Book.RemoveOrderFromID(msg.OrderID); err != nil {
+		return &types.MsgCancelBuyOrderResponse{}, err
+	}
+
+	k.SetBuyOrderBook(ctx, b)
+
+	// refund buyer with remaining price amount
+	buyer, err := sdk.AccAddressFromBech32(order.Creator)
+	if err != nil {
+		return &types.MsgCancelBuyOrderResponse{}, err
+	}
+
+	if err := k.SafeMint(
+		ctx,
+		msg.Port,
+		msg.Channel,
+		buyer,
+		msg.PriceDenom,
+		order.Amount*order.Price,
+	); err != nil {
+		return &types.MsgCancelBuyOrderResponse{}, err
+	}
 
 	return &types.MsgCancelBuyOrderResponse{}, nil
 }
