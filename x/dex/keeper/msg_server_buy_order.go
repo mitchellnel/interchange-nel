f924fe5f26c3597c92954c81f07fe5fc34c430bb
mitchellnel <mitchellmclin@gmail.com>
2022-07-28 12:20:41 +0800

    Implement SendBuyOrder logic

--- x/dex/keeper/msg_server_buy_order.go
+++ x/dex/keeper/msg_server_buy_order.go
@@ -2,27 +2,53 @@ package keeper
 
 import (
 	"context"
+	"errors"
+
+	"interchange-nel/x/dex/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	clienttypes "github.com/cosmos/ibc-go/v3/modules/core/02-client/types"
-	"interchange-nel/x/dex/types"
 )
 
-func (k msgServer) SendBuyOrder(goCtx context.Context, msg *types.MsgSendBuyOrder) (*types.MsgSendBuyOrderResponse, error) {
+func (k msgServer) SendBuyOrder(
+	goCtx context.Context,
+	msg *types.MsgSendBuyOrder,
+) (*types.MsgSendBuyOrderResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: logic before transmitting the packet
+	// cannot send a order if the pair doesn't exist
+	pairIndex := types.OrderBookIndex(msg.Port, msg.ChannelID, msg.AmountDenom, msg.PriceDenom)
+	_, found := k.GetBuyOrderBook(ctx, pairIndex)
+	if !found {
+		return &types.MsgSendBuyOrderResponse{}, errors.New("the pair doesn't exist")
+	}
+
+	// lock the token to send
+	sender, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		return &types.MsgSendBuyOrderResponse{}, err
+	}
+
+	// use SafeBurn to ensure no new native tokens are minted
+	if err := k.SafeBurn(ctx, msg.Port, msg.ChannelID, sender, msg.PriceDenom, msg.Amount*msg.Price); err != nil {
+		return &types.MsgSendBuyOrderResponse{}, err
+	}
+
+	// save the voucher received on the other chain, to have the ability to resolve it into the
+	// original denom
+	k.SaveVoucherDenom(ctx, msg.Port, msg.ChannelID, msg.PriceDenom)
 
 	// Construct the packet
 	var packet types.BuyOrderPacketData
 
+	packet.Buyer = msg.Creator
 	packet.AmountDenom = msg.AmountDenom
 	packet.Amount = msg.Amount
 	packet.PriceDenom = msg.PriceDenom
 	packet.Price = msg.Price
 
 	// Transmit the packet
-	err := k.TransmitBuyOrderPacket(
+	err = k.TransmitBuyOrderPacket(
 		ctx,
 		packet,
 		msg.Port,
