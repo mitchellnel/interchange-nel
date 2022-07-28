86a56270bcb76b3cc4fba5785ebf7140e862573f
mitchellnel <mitchellmclin@gmail.com>
2022-07-28 12:05:10 +0800

    Implement sell order logic -- need to implement types.BuyOrderBook.FillSellOrder function still

--- x/dex/keeper/sell_order.go
+++ x/dex/keeper/sell_order.go
@@ -3,15 +3,17 @@ package keeper
 import (
 	"errors"
 
+	"interchange-nel/x/dex/types"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	clienttypes "github.com/cosmos/ibc-go/v3/modules/core/02-client/types"
 	channeltypes "github.com/cosmos/ibc-go/v3/modules/core/04-channel/types"
 	host "github.com/cosmos/ibc-go/v3/modules/core/24-host"
-	"interchange-nel/x/dex/types"
 )
 
-// TransmitSellOrderPacket transmits the packet over IBC with the specified source port and source channel
+// TransmitSellOrderPacket transmits the packet over IBC with the specified source port and source
+// channel
 func (k Keeper) TransmitSellOrderPacket(
 	ctx sdk.Context,
 	packetData types.SellOrderPacketData,
@@ -23,7 +25,12 @@ func (k Keeper) TransmitSellOrderPacket(
 
 	sourceChannelEnd, found := k.ChannelKeeper.GetChannel(ctx, sourcePort, sourceChannel)
 	if !found {
-		return sdkerrors.Wrapf(channeltypes.ErrChannelNotFound, "port ID (%s) channel ID (%s)", sourcePort, sourceChannel)
+		return sdkerrors.Wrapf(
+			channeltypes.ErrChannelNotFound,
+			"port ID (%s) channel ID (%s)",
+			sourcePort,
+			sourceChannel,
+		)
 	}
 
 	destinationPort := sourceChannelEnd.GetCounterparty().GetPortID()
@@ -38,9 +45,15 @@ func (k Keeper) TransmitSellOrderPacket(
 		)
 	}
 
-	channelCap, ok := k.ScopedKeeper.GetCapability(ctx, host.ChannelCapabilityPath(sourcePort, sourceChannel))
+	channelCap, ok := k.ScopedKeeper.GetCapability(
+		ctx,
+		host.ChannelCapabilityPath(sourcePort, sourceChannel),
+	)
 	if !ok {
-		return sdkerrors.Wrap(channeltypes.ErrChannelCapabilityNotFound, "module does not own channel capability")
+		return sdkerrors.Wrap(
+			channeltypes.ErrChannelCapabilityNotFound,
+			"module does not own channel capability",
+		)
 	}
 
 	packetBytes, err := packetData.GetBytes()
@@ -67,25 +80,104 @@ func (k Keeper) TransmitSellOrderPacket(
 }
 
 // OnRecvSellOrderPacket processes packet reception
-func (k Keeper) OnRecvSellOrderPacket(ctx sdk.Context, packet channeltypes.Packet, data types.SellOrderPacketData) (packetAck types.SellOrderPacketAck, err error) {
+func (k Keeper) OnRecvSellOrderPacket(
+	ctx sdk.Context,
+	packet channeltypes.Packet,
+	data types.SellOrderPacketData,
+) (packetAck types.SellOrderPacketAck, err error) {
 	// validate packet data upon receiving
 	if err := data.ValidateBasic(); err != nil {
 		return packetAck, err
 	}
 
-	// TODO: packet reception logic
+	pairIndex := types.OrderBookIndex(
+		packet.SourcePort,
+		packet.SourceChannel,
+		data.AmountDenom,
+		data.PriceDenom,
+	)
+	book, found := k.GetBuyOrderBook(ctx, pairIndex)
+	if !found {
+		return packetAck, errors.New("The pair does not exist")
+	}
+
+	// fill the sell order
+	remaining, liquidated, gain, _ := book.FillSellOrder(types.Order{
+		Amount: data.Amount,
+		Price:  data.Price,
+	})
+
+	// return the remaining amount and gains
+	packetAck.RemainingAmount = remaining
+	packetAck.Gain = gain
+
+	// before distributing sales, we resolve the denom
+	// first check if the denom received comes from this chain originally
+	finalAmountDenom, saved := k.OriginalDenom(
+		ctx,
+		packet.DestinationPort,
+		packet.DestinationChannel,
+		data.AmountDenom,
+	)
+	if !saved {
+		// if it was not from this chain, we use voucher as denom
+		finalAmountDenom = VoucherDenom(packet.SourcePort, packet.SourceChannel, data.AmountDenom)
+	}
+
+	// dispatch liquidated buy orders
+	for _, liquidation := range liquidated {
+		liquidation := liquidation
+		addr, err := sdk.AccAddressFromBech32(liquidation.Creator)
+		if err != nil {
+			return packetAck, err
+		}
+
+		err = k.SafeMint(
+			ctx,
+			packet.DestinationPort,
+			packet.DestinationChannel,
+			addr,
+			finalAmountDenom,
+			liquidation.Amount,
+		)
+		if err != nil {
+			return packetAck, err
+		}
+	}
+
+	// save the new order book
+	k.SetBuyOrderBook(ctx, book)
 
 	return packetAck, nil
 }
 
 // OnAcknowledgementSellOrderPacket responds to the the success or failure of a packet
 // acknowledgement written on the receiving chain.
-func (k Keeper) OnAcknowledgementSellOrderPacket(ctx sdk.Context, packet channeltypes.Packet, data types.SellOrderPacketData, ack channeltypes.Acknowledgement) error {
+func (k Keeper) OnAcknowledgementSellOrderPacket(
+	ctx sdk.Context,
+	packet channeltypes.Packet,
+	data types.SellOrderPacketData,
+	ack channeltypes.Acknowledgement,
+) error {
 	switch dispatchedAck := ack.Response.(type) {
 	case *channeltypes.Acknowledgement_Error:
+		// in case of error, we mint back the native token
+		receiver, err := sdk.AccAddressFromBech32(data.Seller)
+		if err != nil {
+			return err
+		}
 
-		// TODO: failed acknowledgement logic
-		_ = dispatchedAck.Error
+		err = k.SafeMint(
+			ctx,
+			packet.SourcePort,
+			packet.SourceChannel,
+			receiver,
+			data.AmountDenom,
+			data.Amount,
+		)
+		if err != nil {
+			return err
+		}
 
 		return nil
 	case *channeltypes.Acknowledgement_Result:
@@ -97,7 +189,63 @@ func (k Keeper) OnAcknowledgementSellOrderPacket(ctx sdk.Context, packet channel
 			return errors.New("cannot unmarshal acknowledgment")
 		}
 
-		// TODO: successful acknowledgement logic
+		// get the sell order book
+		pairIndex := types.OrderBookIndex(
+			packet.SourcePort,
+			packet.SourceChannel,
+			data.AmountDenom,
+			data.PriceDenom,
+		)
+		book, found := k.GetSellOrderBook(ctx, pairIndex)
+		if !found {
+			panic("Sell order book must exist")
+		}
+
+		// append the remaining amount of the order
+		if packetAck.RemainingAmount > 0 {
+			_, err := book.AppendOrder(data.Seller, packetAck.RemainingAmount, data.Price)
+			if err != nil {
+				return err
+			}
+
+			// save the new order book
+			k.SetSellOrderBook(ctx, book)
+		}
+
+		// mint the gains
+		if packetAck.Gain > 0 {
+			receiver, err := sdk.AccAddressFromBech32(data.Seller)
+			if err != nil {
+				return err
+			}
+
+			finalPriceDenom, saved := k.OriginalDenom(
+				ctx,
+				packet.SourcePort,
+				packet.SourceChannel,
+				data.PriceDenom,
+			)
+			if !saved {
+				// if it was not from this chain, then we use voucher as denom
+				finalPriceDenom = VoucherDenom(
+					packet.DestinationPort,
+					packet.DestinationChannel,
+					data.PriceDenom,
+				)
+			}
+
+			err = k.SafeMint(
+				ctx,
+				packet.SourcePort,
+				packet.SourceChannel,
+				receiver,
+				finalPriceDenom,
+				packetAck.Gain,
+			)
+			if err != nil {
+				return err
+			}
+		}
 
 		return nil
 	default:
@@ -106,10 +254,32 @@ func (k Keeper) OnAcknowledgementSellOrderPacket(ctx sdk.Context, packet channel
 	}
 }
 
-// OnTimeoutSellOrderPacket responds to the case where a packet has not been transmitted because of a timeout
-func (k Keeper) OnTimeoutSellOrderPacket(ctx sdk.Context, packet channeltypes.Packet, data types.SellOrderPacketData) error {
+// OnTimeoutSellOrderPacket responds to the case where a packet has not been transmitted because of
+// a timeout
+func (k Keeper) OnTimeoutSellOrderPacket(
+	ctx sdk.Context,
+	packet channeltypes.Packet,
+	data types.SellOrderPacketData,
+) error {
+	// in case of error we mint back the native token
+	receiver, err := sdk.AccAddressFromBech32(data.Seller)
+	if err != nil {
+		return err
+	}
 
-	// TODO: packet timeout logic
+	err = k.SafeMint(
+		ctx,
+		packet.SourcePort,
+		packet.SourceChannel,
+		receiver,
+		data.AmountDenom,
+		data.Amount,
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
 
 	return nil
 }
--- x/dex/types/sell_order_book.go
+++ x/dex/types/sell_order_book.go
@@ -8,3 +8,84 @@ func NewSellOrderBook(amountDenom string, priceDenom string) SellOrderBook {
 		Book:        &book,
 	}
 }
+
+func (s *SellOrderBook) AppendOrder(creator string, amount int32, price int32) (int32, error) {
+	return s.Book.appendOrder(creator, amount, price, Decreasing)
+}
+
+func (s *SellOrderBook) FillBuyOrder(
+	order Order,
+) (remainingBuyOrder Order, liquidated []Order, purchase int32, filled bool) {
+	var liquidatedList []Order
+	totalPurchase := int32(0)
+	remainingBuyOrder = order
+
+	// liquidate as long as there is a match
+	for {
+		var match bool
+		var liquidation Order
+
+		remainingBuyOrder, liquidation, purchase, match, filled = s.LiquidateFromBuyOrder(
+			remainingBuyOrder,
+		)
+		if !match {
+			break
+		}
+
+		// update gains
+		totalPurchase += purchase
+
+		// update liquidated
+		liquidatedList = append(liquidatedList, liquidation)
+
+		if filled {
+			break
+		}
+	}
+
+	return remainingBuyOrder, liquidatedList, totalPurchase, filled
+}
+
+func (s *SellOrderBook) LiquidateFromBuyOrder(
+	order Order,
+) (remainingBuyOrder Order, liquidatedSellOrder Order, purchase int32, match bool, filled bool) {
+	remainingBuyOrder = order
+
+	// no match if no order
+	orderCount := len(s.Book.Orders)
+	if orderCount == 0 {
+		return order, liquidatedSellOrder, purchase, false, false
+	}
+
+	// check if match
+	lowestAsk := s.Book.Orders[orderCount-1]
+	if order.Price < lowestAsk.Price {
+		return order, liquidatedSellOrder, purchase, false, false
+	}
+
+	liquidatedSellOrder = *lowestAsk
+
+	// check if buy order can be entirely filled
+	if lowestAsk.Amount >= order.Amount {
+		remainingBuyOrder.Amount = 0
+		liquidatedSellOrder.Amount = order.Amount
+		purchase = order.Amount
+
+		// remove lowest ask if it has been entirely liquidated
+		lowestAsk.Amount -= order.Amount
+		if lowestAsk.Amount == 0 {
+			s.Book.Orders = s.Book.Orders[:orderCount-1]
+		} else {
+			s.Book.Orders[orderCount-1] = lowestAsk
+		}
+
+		return remainingBuyOrder, liquidatedSellOrder, purchase, true, true
+	}
+
+	// not entirely filled
+	purchase = lowestAsk.Amount
+	s.Book.Orders = s.Book.Orders[:orderCount-1]
+	remainingBuyOrder.Amount -= lowestAsk.Amount
+
+	return remainingBuyOrder, liquidatedSellOrder, purchase, true, false
+}
